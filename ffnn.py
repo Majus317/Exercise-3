1bc442bd9c43795b700c8bd5f9079deb931b0662
Janis Pagel <janis.pagel@uni-koeln.de>
2024-10-24 11:47:13 +0200

    Add exercise

---
+++ ffnn.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+Implementation of simple feed forward neural network
+"""
+
+
+def ReLU(x):
+    pass
+
+
+def sigmoid(x):
+    pass
+
+
+def softmax(x):
+    pass
+
+
+def FFNN(input_vector, mode="binary"):
+    pass
